1f61bcafe8ce205a3ccb80f3b708c58a2a47d8cc
KyEdwards <123298031+KyEdwards@users.noreply.github.com>
2024-12-05 16:38:23 +0000

    Basic CRUD functionality added to the products table

--- controlers/get.products.js
+++ controlers/get.products.js
@@ -1,7 +1,7 @@
 
 const models = require('../models')
 
-
+//---------------------------------------//
 //Function to add a Singular product
 function save(req, res){
     const product = {
@@ -24,24 +24,87 @@ models.products.create(product).then(result => {
 });
 }
 
-
+//---------------------------------------//
 // Function to show a singular product based on an input of the item's ID
 function show(req, res){
     const id = req.params.id;
 
     models.products.findByPk(id).then(result =>{
-        res.status(200).json(result);
+        if(result){
+            res.status(200).json(result);
+        }else{
+            res.status(404).json({
+             message: "Something went wrong!, Product Id not found :(" 
+            })
+        }
     })
     .catch(error =>{
         res.status(500).json({
-            message: "Something went wrong"
+            message: "Something went wrong."
         })
     });
 }
 
+//---------------------------------------//
+//Function to return all of the products in the table and their row values. 
+function index( req, res){
+    models.products.findAll().then(result =>{
+        res.status(200).json(result);
+    }).catch(err =>{
+        res.status(500).json({
+            message: "Something went wrong."
+        })
+    });
+}
 
+//---------------------------------------//
+//Function to update a singular product. 
+function update(req, res){
+    const id = req.params.id;
+    const updatedProduct = {
+        product_name: req.body.product_name,
+        price: req.body.price,
+        quantity_avalable: req.body.quantity_avalable
+    }
+    models.products.update(updatedProduct, {where: {id:id}}).then(result=>{
+        res.status(200).json({
+            message: "Product Updated", 
+            product: updatedProduct
+        });
+    }).catch(error =>{
+        res.status(200).json({
+            message: "Something went wrong", 
+            error: error
+        });
+    })
+}
+
+//---------------------------------------//
+// Function to remove a product from the databse. 
+function removeProduct(req,res){
+    const id = req.params.id;
+
+    models.products.destroy({where: {id:id}}).then(resutlt =>{
+        res.status(200).json({
+            message: "Product Removed", 
+        });
+    }).catch(error =>{
+        res.status(200).json({
+            message: "Something went wrong", 
+            error: error
+        });
+    })
+}
+
+
+
+
+//---------------------------------------//
 module.exports = {
 
     save:save,
-    show:show
+    show:show,
+    index:index,
+    update:update,
+    removeProduct:removeProduct
 };
--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,9 @@ const productControler = require ('../controlers/get.products');
 const router = express.Router();
 
 router.post("/", productControler.save); //Add a product
-router.get("/:id", productControler.show); //Find a product via its ID. 
+router.get("/:id", productControler.show); //Find a product via its ID. THE : is a placeholder for the endpoints data sent. 
+router.get("/", productControler.index); //Gets all the products in the product table. 
+router.patch("/:id", productControler.update); //Update a product with it's ID in the endpoint and the updated values in a JSON format.
+router.delete("/:id", productControler.removeProduct); //Removes a product from the table via its ID in the endpoint. 
 
 module.exports = router; 
